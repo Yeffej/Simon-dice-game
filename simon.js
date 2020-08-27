d323b0bf9026de71b1f26223b8821eb5f7eedf91
Yefri Encarnación <yefriej128@gmail.com>
2020-08-26 21:21:00 -0400

    Se añadieron elementos en js para hacer interactivo al boton

--- simon.js
+++ simon.js
@@ -1,3 +1,26 @@
-function startThegame() {
-    return Game()
+const button = document.querySelector("#btstart")
+const red = document.querySelector("#red")
+const green = document.querySelector("#green")
+const violet = document.querySelector("#violet")
+const purple = document.getElementById("purple")
+const colorsTable = document.getElementById("colorsTable")
+const colorsTableStyle = window.getComputedStyle(colorsTable)
+const colorsTableValue = colorsTableStyle.getPropertyValue("display") 
+
+class game {
+    constructor() {
+        // this.initial()
+        this.gameboard()
+    }
+    gameboard () {
+        button.classList.add("hide")
+        if (colorsTableValue === "none"){
+            colorsTable.style.setProperty("display", "initial")
+        }
+    }
+}
+
+function StartThegame() {
+    let Game
+     Game = new game()
 }
