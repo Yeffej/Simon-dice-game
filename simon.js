569429b10132f1deac903f59801c9525c2ce62d6
Yefri Encarnación <yefriej128@gmail.com>
2020-09-03 02:13:35 -0400

    Se termina de hacer el gameboard interactivo y se cambian algunas de sus propiedades de css para la causa.

--- simon.js
+++ simon.js
@@ -9,39 +9,63 @@ const colorsTableValue = colorsTableStyle.getPropertyValue("display")
 
 class game {
     constructor() {
+        // this.randomShow = this.randomShow.bind(game)
         this.gameboardStart()
+        this.randomShow()
+        this.nextLevel()
     }
-    gameboardStart () {
-        button.classList.add("hide")
-        if (colorsTableValue === "none"){
-            colorsTable.style.setProperty("display", "initial")
+        gameboardStart () {
+            button.classList.add("hide")
+            if (colorsTableValue === "none"){
+                colorsTable.style.setProperty("display", "initial")
+            } 
+            this.level = 7
+            this.colors = {
+                red, 
+                green,
+                violet,
+                purple,
+            }
         }
-        showAndClick()
+        nextLevel () {
+            this.shine()
+        }
+        transformAColor(number) {
+            
+            switch (number) {
+                case 0: 
+                    return "red";
+                case 1:
+                    return "green";
+                case 2: 
+                    return "violet";
+                case 3: 
+                    return "purple";
+            }
+        }
+        
+        randomShow() {
+            this.showAcolor = new Array(12).fill(0).map(n => n = Math.floor(Math.random()*4))
+
+        }
+        shine() {
+            for (let i = 0; i < this.level; i++) {
+                const colorChose = this.transformAColor(this.showAcolor[i])
+                setTimeout(() => this.turnOn(colorChose), 2000*i) 
+            }      
+        }
+        turnOn(color) {
+            this.colors[color].classList.add("light")
+            setTimeout(() => this.tunroff(color), 1000);
+             
+        }
+        tunroff(color) {
+            this.colors[color].classList.remove("light")
+        }
     }
-    showAndClick() {
-        randomShow()
-    }
-    randomShow() {
-        let colorchose
-       const randomNumber = Math.floor(Math.random()* 4)
-       switch (randomNumber) {
-           case 0:
-                colorchose = red
-                break;
-            case 1: 
-                colorchose = green
-                break;
-            case 2: 
-                colorchose = purple
-                break;
-            case 3: 
-                colorchose = violet
-       }
-       return colorchose
-    }
-}
+
 
 function StartThegame() {
     let Game
-     Game = new game()
+    Game = new game()
 }
