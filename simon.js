d0e3125a21db890c5aab04f955cab5066fc0e08e
Yefri Encarnación <yefriej128@gmail.com>
2020-09-10 00:58:48 -0400

    Se termina el Juego Simon dice, faltando solo darle algunos retoques finales V1.0

--- simon.js
+++ simon.js
@@ -5,36 +5,46 @@ const violet = document.querySelector("#violet")
 const purple = document.getElementById("purple")
 const colorsTable = document.getElementById("colorsTable")
 const colorsTableStyle = window.getComputedStyle(colorsTable)
-const colorsTableValue = colorsTableStyle.getPropertyValue("display") 
+const colorsTableValue = colorsTableStyle.getPropertyValue("display")
+const colorsAttribute = colorsTable.getAttribute("style") 
 const audio = document.getElementById("audio1")
 const audio2 = document.getElementById("audio2")
 const audio3 = document.getElementById("audio3")
 const audio4 = document.getElementById("audio4")
-
+const max_level = 3 
 
 class game {
     constructor() {
-        // this.randomShow = this.randomShow.bind(game)
         this.gameboardStart()
         this.randomShow()
-        this.nextLevel()
+        setTimeout(()=> this.nextLevel(), 500 )   
     }
         gameboardStart () {
-            button.classList.add("hide")
-            if (colorsTableValue === "none"){
-                colorsTable.style.setProperty("display", "initial")
-            } 
-            this.level = 7
+            this.checkOutEvent = this.checkOutEvent.bind(this)
+            button.classList.toggle("hide")
+            this.hideTheGameboard()
+            // console.log(swal)
+            this.level = 1
             this.colors = {
                 red, 
                 green,
                 violet,
                 purple,
             }
-            this.audios = {audio, audio2, audio3, audio4,}
+            this.audios = {audio, audio2, audio3, audio4,} 
+        }
+        hideTheGameboard() {
+            const style = colorsTable.style.display
+            if(style === "initial") {
+                colorsTable.style.setProperty("display", "none")
+            }else {
+                colorsTable.style.setProperty("display", "initial")
+            }
         }
         nextLevel () {
             this.shine()
+            this.sublevel = 0
+            this.addinglisteners()
         }
         transformAColor(number) {
             
@@ -51,7 +61,7 @@ class game {
         }
         
         randomShow() {
-            this.showAcolor = new Array(12).fill(0).map(n => n = Math.floor(Math.random()*4))
+            this.randomNumbers = new Array(max_level).fill(0).map(n => n = Math.floor(Math.random()*4))
 
         }
         transformAaudio(color) {
@@ -68,10 +78,10 @@ class game {
         }
         shine() {
             for (let i = 0; i < this.level; i++) {
-                const colorChose = this.transformAColor(this.showAcolor[i])
+                const colorChose = this.transformAColor(this.randomNumbers[i])
                 const audioChose = this.transformAaudio(colorChose)
-                console.log(audioChose)
-                setTimeout(() => this.turnOn(colorChose, audioChose), 3000*i) 
+                // console.log(audioChose)
+                setTimeout(() => this.turnOn(colorChose, audioChose), 2800*i) 
             }      
         }
         turnOn(color, audiox) {
@@ -82,6 +92,78 @@ class game {
         tunroff(color) {
             this.colors[color].classList.remove("light")
         }
+        levelup() {
+            // this.level ++
+            if (this.level !== max_level) {
+                this.level ++
+               setTimeout(() => this.nextLevel(), 2000); 
+            }else {
+                this.removinglisteners()
+                swal("Felicidades!, ¡Ganaste el juego!", "Completaste todos los niveles", "success")
+                .then(this.gameboardStart())
+                .catch(() => alert("Lo siento ha habido un error del sistema"))
+            }
+
+        }
+        turnclick(clickedColor) {
+            this.colors[clickedColor].classList.add("click")
+            const audioClicked = this.transformAaudio(clickedColor)
+            this.audios[audioClicked].play()
+            setTimeout(() => this.turnClickOff(clickedColor), 800)
+        }
+        turnClickOff(click) {
+            this.colors[click].classList.remove("click")
+        }
+        transformANumber(color) {
+            switch (color) {
+                case "red": 
+                    return 0;
+                case "green":
+                    return 1;
+                case "violet": 
+                    return 2;
+                case "purple": 
+                    return 3;
+            }
+        }
+
+        checkOutEvent(ev) {
+            const clickedColor = ev.target.dataset.color
+            this.turnclick(clickedColor)
+            const clickedColorNumber = this.transformANumber(clickedColor)
+            if (clickedColorNumber === this.randomNumbers[this.sublevel]) {
+                this.sublevel ++
+                if (this.level === this.sublevel) {
+                    this.levelup()
+                    this.removinglisteners()
+                }
+            }else {
+                this.removinglisteners()
+                // if (swal) {
+                    swal("ups, Perdiste el juego", "Intentalo de nuevo", "error")
+                    .then(this.gameboardStart())
+                    .catch(() => alert("Lo sentimos ha habido un error del sistema"))
+                // } else {
+                //     alert("Lo sentimos al parecer ha ocurrido un error en la conexión")
+                // }
+    
+            }  
+            
+        }
+
+        addinglisteners() {
+            red.addEventListener("click", this.checkOutEvent)
+            purple.addEventListener("click", this.checkOutEvent)
+            violet.addEventListener("click", this.checkOutEvent)
+            green.addEventListener("click", this.checkOutEvent)
+        }
+        removinglisteners() {
+            red.removeEventListener("click", this.checkOutEvent)
+            purple.removeEventListener("click", this.checkOutEvent)
+            violet.removeEventListener("click", this.checkOutEvent)
+            green.removeEventListener("click", this.checkOutEvent)
+        }
+      
     }
 
 
